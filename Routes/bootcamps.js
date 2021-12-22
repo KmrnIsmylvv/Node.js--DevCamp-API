8b81d23720e51f940e2df0af1bb2aae5f923b65f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-22 13:58:30 +0400

    feat: created Controller methods

--- Routes/bootcamps.js
+++ Routes/bootcamps.js
@@ -1,28 +1,26 @@
 const express = require('express');
+const { route } = require('express/lib/application');
+const { get } = require('express/lib/request');
+
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+router
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp);
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
-
-module.exports=router;
+module.exports = router;
