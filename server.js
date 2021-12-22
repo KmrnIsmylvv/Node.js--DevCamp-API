6070f846ffa23948c6d500c24d48bd6a6b981087
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-22 11:51:57 +0400

    refactor: created rootes and responses

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const res = require('express/lib/response');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
-const app=express();
+const app = express();
 
-const PORT=process.env.PORT || 5000;
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
