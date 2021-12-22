05261a75ac6024aa5643b3d11c04a26cc29125ac
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-23 00:08:49 +0400

    feat: created middleware

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 
 //Route files
-const bootcamps=require('./Routes/bootcamps');
+const bootcamps = require('./Routes/bootcamps');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-//Mount routers
-app.use('/api/v1/bootcamps',bootcamps);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
