403575a451b823212e1b1c69d2a606b875a8a67a
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-26 14:17:20 +0400

    feat: added custom error Response message

--- middleware/error.js
+++ middleware/error.js
@@ -2,10 +2,10 @@ const errorHandler = (err, req, res, next) => {
   // Log to console for dev
   console.log(err.stack.red);
 
-  res.status(500).json({
+  res.status(err.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: err.message || 'Server Error',
   });
 };
 
-module.exports=errorHandler;
+module.exports = errorHandler;
