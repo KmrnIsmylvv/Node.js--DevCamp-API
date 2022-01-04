090f7361526ae42a31aaa43b89f20a4128c7fe65
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-04 07:24:39 +0400

    feat: added pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -44,12 +44,43 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startindex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startindex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
 
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startindex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
   res
     .status(200)
-    .json({ succes: true, count: bootcamps.length, data: bootcamps });
+    .json({
+      succes: true,
+      count: bootcamps.length,
+      pagination,
+      data: bootcamps,
+    });
 });
 
 // @desc    Get single bootcamps
