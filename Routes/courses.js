ba1b14f412d5858ab32b644fbc58c9c6cb2160ae
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-08 17:24:40 +0400

    feat: update and delete courses

--- Routes/courses.js
+++ Routes/courses.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
