dbce5092752eff130b6080bd5f1ae7499d77a179
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-09 08:00:46 +0400

    feat: Calculating average course cost

--- seeder.js
+++ seeder.js
@@ -29,7 +29,7 @@ const courses = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
+    //await Course.create(courses);
 
     console.log('Data imported...'.green.inverse);
     process.exit();
