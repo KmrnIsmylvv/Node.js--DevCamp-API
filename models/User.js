94070faffaab26363b64669eb80455bbb00c8fbf
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-22 15:14:05 +0400

    feat: User Register _ Encrypting Passwords

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -33,4 +34,10 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-module.exports=mongoose.model('User',UserSchema);
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', UserSchema);
