5acd0402b5b2c6caa8d6d9d3fa5b3bf2972fd4c4
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-23 16:54:12 +0400

    feat: Sign and Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,5 +16,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
