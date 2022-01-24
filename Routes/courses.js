494731fb4b7ebb2c14279ff16467ecffacafdace
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-24 14:28:25 +0400

    feat: added Auth Protect Middleware

--- Routes/courses.js
+++ Routes/courses.js
@@ -13,16 +13,23 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
     advancedResults(Course, {
       path: 'bootcamp',
-      select: 'name description',}),
+      select: 'name description',
+    }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, addCourse);
 
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
