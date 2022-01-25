0094e8925ad6509a2409e73d3bc54f9530a6b906
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-25 15:20:19 +0400

    refactor: added Course Ownership logic into courses controller

--- models/Course.js
+++ models/Course.js
@@ -40,6 +40,11 @@ const CourseSchema = new mongoose.Schema({
     ref: 'Bootcamp',
     required: true,
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 // Static method to get avg of course tuitions
