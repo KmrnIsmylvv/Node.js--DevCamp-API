f860f628377e8867ec901b8077c118c02ed6b875
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-25 16:29:52 +0400

    feat: added Forgot Password method and generate token

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,5 +13,6 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', protect, login);
 router.get('/me', protect, getMe);
+router.post('/forgotPassword', forgotPassword);
 
 module.exports = router;
--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Get current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Forgot password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -68,15 +101,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   });
 };
-
-// @desc    Get current logged in user
-// @route   POST /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
