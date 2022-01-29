b6d0c60a76965af3187218ef01920f96223a9ef7
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-29 20:16:58 +0400

    feat: added  delete and update Reviews methods

--- Routes/reviews.js
+++ Routes/reviews.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
-const { getReviews, getReview, addReview } = require('../controllers/reviews');
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -20,6 +26,10 @@ router
   )
   .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
