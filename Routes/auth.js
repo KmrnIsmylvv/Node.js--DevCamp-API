abe4eba080023e61af5125f47dc7da6040390c51
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-29 22:12:11 +0400

    feat: Logout To Clear Token Cookie

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -15,6 +16,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', protect, login);
+router.get('/logout', protect, logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
   }
-
-  // else if(req.cookies.token){
-  //     token=req.cookies.token
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
@@ -43,7 +44,10 @@ exports.authorize = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorResponse(
-          `User role ${req.user.role} is not authorized to access this route`, 403));
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
     }
     next();
   };
